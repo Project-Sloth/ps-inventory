05191c30030f30ec93f334d0eac255148493918f
vanila1985 <151553693+vanila1985@users.noreply.github.com>
2023-11-20 21:16:13 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,35 +1,36 @@
 fx_version 'cerulean'
 game 'gta5'
-
-description 'ps-inventory'
-version '1.0.0'
+lua54 'yes'
+description 'QB-Inventory'
+version '1.2.4'
 
 shared_scripts {
-	'config.lua',
-	'@qb-weapons/config.lua'
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
 }
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-	'server/main.lua'
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
 }
 
 client_script 'client/main.lua'
 
 ui_page {
-	'html/ui.html'
+    'html/ui.html'
 }
 
 files {
-	'html/ui.html',
-	'html/css/main.css',
-	'html/js/app.js',
-	'html/images/*.svg',
-	'html/images/*.png',
-	'html/images/*.jpg',
-	'html/ammo_images/*.png',
-	'html/attachment_images/*.png',
-	'html/*.ttf'
+    'html/ui.html',
+    'html/css/main.css',
+    'html/js/app.js',
+    'html/images/*.png',
+    'html/images/*.jpg',
+    'html/ammo_images/*.png',
+    'html/attachment_images/*.png',
+    'html/*.ttf'
 }
 
-lua54 'yes'
+dependency 'qb-weapons'
