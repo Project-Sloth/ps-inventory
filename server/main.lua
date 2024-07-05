010ec2fd96ac2ef75f1338a2e8eb068c052a3b47
James King <james@jamesking.dev>
2024-07-05 20:48:40 +0100

    Rename event prefix to add ps-

--- server/main.lua
+++ server/main.lua
@@ -1030,7 +1030,7 @@ local function OpenInventory(name, id, other, origin)
 					Stashes[id].isOpen = src
 					Stashes[id].label = secondInv.label
 				end
-				TriggerClientEvent('inventory:client:SetCurrentStash', src, id)
+				TriggerClientEvent('ps-inventory:client:SetCurrentStash', src, id)
 			end
 		elseif name == "trunk" then
 			if Trunks[id] then
